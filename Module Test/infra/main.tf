dd17e94e9130030c807a55945dfdf1b1e39cddd7
fortisfortunaadiuvat <ashif.barkhiyya@tokopedia.com>
2021-12-03 09:18:43 +0700

    git commit

--- Module Test/infra/main.tf
+++ Module Test/infra/main.tf
@@ -9,3 +9,15 @@ terraform {
 provider "aws" {
   region = "us-west-2" # Oregon
 }
+
+resource "aws_vpc" "web_vpc" {
+  cidr_block = "192.168.100.0/24"
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "Web VPC"
+  }
+}
+
+#aws ec2 describe-vpcs --region us-west-2 --filter "Name=tag:Name,Values=Web VPC"
+#aws ec2 describe-vpc-attribute --region us-west-2 --attribute enableDnsHostnames --vpc-id <VPC_ID>
