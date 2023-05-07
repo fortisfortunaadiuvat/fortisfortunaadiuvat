532baa2df95335fb6555ef5cf8ff2c8f17189b45
fortisfortunaadiuvat <94946336+fortisfortunaadiuvat@users.noreply.github.com>
2023-05-07 18:11:19 +0700

    Update diary-flex-1d-collector.sh

--- JsonScripttest/diary-flex-1d-collector.sh
+++ JsonScripttest/diary-flex-1d-collector.sh
@@ -72,7 +72,7 @@ collect_os_dpkg() {
         "diaryEventType=diary_flex" \
         "diaryEventSourceType=diary_flex_os_dpkg" \
         "diaryEventActor=diary-flex-1d-collector.sh" \
-        "Packages=$OS_DPKG_JSON"
+        "Packages='$OS_DPKG_JSON'"
 }
 
 debug_event_params() {
